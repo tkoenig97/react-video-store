650bfd806fc3452f77e196c66841c6d9822cf33e
tkoenig97 <tkoenig97@github.com>
2023-03-27 13:55:46 -0500

    Removed fragment

--- src/components/InventoryItem.jsx
+++ src/components/InventoryItem.jsx
@@ -1,17 +1,15 @@
 export default function InventoryItem({ title, imgUrl, copiesAvailable }) {
     return (
-        <>
-            <div className="inventoryItem">
-                <h2>{title}</h2>
-                <img src={imgUrl} />
-                <p>Copies Available: {copiesAvailable}</p>
-                <button
-                    disabled={copiesAvailable === 0}
-                    onClick={() => alert(`You checked out ${title}`)}
-                >
-                    Check Out
-                </button>
-            </div>
-        </>
+        <div className="inventoryItem">
+            <h2>{title}</h2>
+            <img src={imgUrl} />
+            <p>Copies Available: {copiesAvailable}</p>
+            <button
+                disabled={copiesAvailable === 0}
+                onClick={() => alert(`You checked out ${title}`)}
+            >
+                Check Out
+            </button>
+        </div>
     );
 }
