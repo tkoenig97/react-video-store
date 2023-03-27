cb365cae378e8906106aee777cbe74b78a746bb4
tkoenig97 <tkoenig97@github.com>
2023-03-27 11:48:26 -0500

    Created inventory component

---
+++ src/components/InventoryContainer.jsx
@@ -0,0 +1,13 @@
+export default function InventoryContainer() {
+    return (
+        <div className="inventory-container">
+            <h2>Inventory</h2>
+            <div className="inventory-list">
+                <div>A</div>
+                <div>B</div>
+                <div>C</div>
+                <div>D</div>
+            </div>
+        </div>
+    );
+}
