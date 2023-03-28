3706515c4c8ee652c2b668ed17c0d470d838742a
tkoenig97 <tkoenig97@github.com>
2023-03-28 14:53:06 -0500

    Added detail panel elements and CSS

--- src/components/DetailsPanel.jsx
+++ src/components/DetailsPanel.jsx
@@ -4,7 +4,29 @@ export default function DetailsPanel({ selectedTitle, setSelectedTitleById }) {
     return (
         <div className="details_panel">
             <h2>Details</h2>
-            <button onClick={() => setSelectedTitleById(null)}>Hide Details</button>
+            <div className="basic_container_column">
+                <h3>{title}</h3>
+                <div className="basic_container_row align_center">
+                    <img src={imgUrl} />
+                    <p>{description}</p>
+                    <pre style={{ fontSize: "40px" }}>{rating}</pre>
+                </div>
+                <div className="basic_container_column">
+                    {copiesAvailable.current} / {copiesAvailable.total} available
+                    <div className="basic_container_row">
+                        <button
+                            disabled={copiesAvailable.current === 0}
+                            onClick={() => checkoutOrReturnFilmById(id, "checkout")}
+                        >Check out</button>
+                        <button
+                            disabled={copiesAvailable.current === copiesAvailable.total}
+                            onClick={() => checkoutOrReturnFilmById(id, "return")}
+                        >Return</button>
+                        <button onClick={() => setSelectedTitleById(null)}>Hide Details</button>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
