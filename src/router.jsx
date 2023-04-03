caa8ee88892afcb11f6ad4f83209ce56eb654029
tkoenig97 <tkoenig97@github.com>
2023-04-03 14:25:59 -0500

    Created router file

---
+++ src/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage, { allFilmsLoader } from './components/HomePage';
+import DetailsPage, { filmLoader } from './components/DetailsPage';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+                loader: allFilmsLoader,
+            },
+            {
+                path: 'film/:filmId',
+                element: <DetailsPage />,
+                loader: filmLoader,
+            },
+        ],
+    },
+]);
+
+export default router;
